f5aa95f39c3dfeedde674b8a919b2a14819ed94c
Andri Wicaksono <andriwicaksonost@gmail.com>
2020-08-08 16:24:38 +0700

    6. Tampilkan data di Livewire

--- app/Http/Livewire/Post/Index.php
+++ app/Http/Livewire/Post/Index.php
@@ -3,11 +3,21 @@
 namespace App\Http\Livewire\Post;
 
 use Livewire\Component;
+use Livewire\WithPagination;
+use App\Post;
 
 class Index extends Component
 {
+	use WithPagination;
+
+	// public $posts;
+
     public function render()
     {
-        return view('livewire.post.index');
+    	// $this->posts = Post::latest()->paginate(10);
+
+        return view('livewire.post.index', [
+        	'posts' => Post::latest()->paginate(5)
+        ]);
     }
 }
--- resources/views/livewire/post/index.blade.php
+++ resources/views/livewire/post/index.blade.php
@@ -1,3 +1,29 @@
 <div>
-	Hello
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<button href="{{ route('post.create') }}" class="btn btn-success btn-md mb-3">Tambah</button>
+				<table class="table">
+				  <thead class="thead-dark">
+				    <tr>
+				      <th scope="col">Title</th>
+				      <th scope="col">Content</th>
+				      <th scope="col">Action</th>
+				    </tr>
+				  </thead>
+				  <tbody>
+				  	@foreach($posts as $post)
+				    <tr>
+				      <td>{{ $post->title }}</td>
+				      <td>{{ $post->content }}</td>
+				      <td>
+				      	
+				      </td>
+				    </tr>
+				    @endforeach
+				  </tbody>
+				</table>
+			</div>
+		</div>
+	</div>
 </div>
