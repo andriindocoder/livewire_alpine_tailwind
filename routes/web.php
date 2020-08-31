430a119124889c1fd826d08ce8d3c4ce4348e09b
Andri Wicaksono <andriwicaksonost@gmail.com>
2020-08-31 09:17:46 +0700

    1a. Counter save to db

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/counter', function () {
+    return view('counter');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
